3d04d21da4714b02e522db7a72e38314e9a9e60e
Lawrence Jones <git@lwjon.es>
2022-01-11 15:45:07 -0800

    Create task elements from form contents

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,21 @@ let tasksToDoEl = document.querySelector("#tasks-to-do");
 let createTaskHandler = function(event) {
     event.preventDefault();
 
+    // get values from form
+    let taskNameInput = document.querySelector("input[name='task-name']").value;
+    let taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // create list item
     let listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // create div to hold task info and add to list item
+    let taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    // add task to list
     tasksToDoEl.appendChild(listItemEl);
 }
 
